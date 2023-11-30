81a5525fa04e903257391383925fe96ebdd5238a
ShikoliAmos31 <amosshikoli@gmail.com>
2023-11-30 11:47:09 -0800

    How to make a script dynamic!

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,7 @@
 if __name__ == "__main__":
     import sys, math
     result = 0
-    for i in sys.argv[1:]:
-        try:
-            result += int(i)
-            print("{}".format(result))
-        except valueError:
-            print(f"Error: {i} is not a valid integer.")
+    for i in sys.argv:
+        result += int(i)
+        print("{}".format(result))
 
