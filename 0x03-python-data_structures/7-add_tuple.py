e0d2d910add830c4c70ed02553ba59b7eb78fc98
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-03 23:57:59 -0800

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,20 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-
-    # Only consider the first 2 integers in each tuple
-    result = (a[0] + b[0], a[1] + b[1])
-
-    return result
-
-
-# Test cases
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-
-print(add_tuple(tuple_a, (1,)))
-print(add_tuple(tuple_a, ()))
+    new_tuple = (a[0] + b[0], a[1] + b[1])
+    return new_tuple
