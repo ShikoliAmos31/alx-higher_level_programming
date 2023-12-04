88d7904c0e491d9bc9eef53de2f7c734ad96a8c7
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-04 12:10:48 -0800

    CPython #0: Python lists

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <Python.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -24,5 +24,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-
+void print_python_list_info(PyObject *p);
 #endif /* LISTS_H */
