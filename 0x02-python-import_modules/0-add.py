3ccdd823a8de6683451247eb28956710204d8beb
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-05 05:17:38 -0800

    Infinite addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
+    try:
+        from add_0 import add
+    except ImportError:
+        print("Module 'add_0' not found. Make sure the file exists and the name is correct.")
+    else:
+        a = 1
+        b = 2
 
-    a = 1
-    b = 2
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
+        # The variables 'a' and 'b' should be defined outside the 'else' block
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
