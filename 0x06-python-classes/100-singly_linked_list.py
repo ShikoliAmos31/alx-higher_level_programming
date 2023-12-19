6b5bde1a591383d65d891c2dc9a844c5c023b36a
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-19 02:14:21 -0800

    Singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""Define a Node class and a SinglyLinkedList class."""
+
+class Node:
+    """Represents a node in a singly linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+
+        Args:
+            data (int): The data for the new node.
+            next_node (Node): The next node in the linked list.
+
+        Raises:
+            TypeError: If data is not an integer or next_node is not a Node object.
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Get the data of the node."""
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        """Set the data of the node.
+
+        Args:
+            value (int): The new data value.
+
+        Raises:
+            TypeError: If value is not an integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self._data = value
+
+    @property
+    def next_node(self):
+        """Get the next node in the linked list."""
+        return self._next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Set the next node in the linked list.
+
+        Args:
+            value (Node): The next node.
+
+        Raises:
+            TypeError: If value is not a Node object.
+        """
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self._next_node = value
+
+
+class SinglyLinkedList:
+    """Represents a singly linked list."""
+
+    def __init__(self):
+        """Initialize an empty singly linked list."""
+        self.head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node into the correct sorted position in the list (increasing order).
+
+        Args:
+            value (int): The value to be inserted.
+        """
+        new_node = Node(value)
+
+        if self.head is None or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next_node is not None and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
+
+    def display(self):
+        """Print the entire list in stdout, one node number by line."""
+        result = ""
+        current = self.head
+        while current:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        print(result.strip())
