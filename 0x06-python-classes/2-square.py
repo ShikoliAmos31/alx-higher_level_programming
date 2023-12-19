fe86e2114dc7e57b11619e736214ed1118827f3d
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-19 09:23:38 -0800

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,65 +1,17 @@
 #!/usr/bin/python3
-"""
-Module 2-square
-
-Defines a Square class with private instance attribute size and validation.
-"""
+"""Define a class Square."""
 
 
 class Square:
-    """
-    Represents a square.
-
-    Attributes:
-        size (int): The size of the square.
-    """
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """
-        Initializes a new Square.
-
-        Parameters:
-            size (int): The size of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
