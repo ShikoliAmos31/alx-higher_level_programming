54a9980a9332ffeb5225c22cb7c11969a12852aa
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-19 01:30:11 -0800

    Square with size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,43 +1,14 @@
 #!/usr/bin/python3
+"""Module defining a Square class"""
+
+
 class Square:
-    def __init__(self, size=0):
+    """Square class with a private instance attribute: size"""
+
+    def __init__(self, size):
+        """Initialize a new Square instance
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
