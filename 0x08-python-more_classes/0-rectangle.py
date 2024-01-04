0f93e0c2fb04880274d637037b9592b4a98f90c5
ShikoliAmos31 <amosshikoli@gmail.com>
2024-01-04 11:10:24 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a rectangle class."""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    ""REpresent a rectangle.""
+    """Represent a rectangle."""
     pass
