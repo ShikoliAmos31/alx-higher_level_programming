ede29a3828cc35d44c16c37ce60a27de3d0bc12e
ShikoliAmos31 <amosshikoli@gmail.com>
2024-01-08 12:03:55 -0800

    all of the 0x0A. Python - Inheritance projects

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
 class MyInt(int):
-    """A custom class inheriting from int."""
+    """Class representing a custom integer type."""
 
     def __eq__(self, other):
-        """Override equality (==) to be inequality (!=)."""
-        return super().__ne__(other)
+        """Override equality comparison."""
+        return int(self) != other
 
     def __ne__(self, other):
-        """Override inequality (!=) to be equality (==)."""
-        return super().__eq__(other)
+        """Override inequality comparison."""
+        return int(self) == other
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        super().__init__(size, size)
