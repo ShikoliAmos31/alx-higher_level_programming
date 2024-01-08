d40c15999a08b0643af662493d18e73152e1264f
ShikoliAmos31 <amosshikoli@gmail.com>
2024-01-08 06:09:53 -0800

    all of the 0x0A. Python - Inheritance projects

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a base geometry a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """represent base geometry."""
+
+    def area(self):
+        """not implemented."""
+        raise Exception("area() is not implemented")
