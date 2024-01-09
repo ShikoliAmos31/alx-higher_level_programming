9997ab17cc5daab3c040ab021b9e33d132f9be99
ShikoliAmos31 <amosshikoli@gmail.com>
2024-01-09 11:04:18 -0800

    Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-add_attribute = __import__('101-add_attribute').add_attribute
 
-class MyClass():
-    pass
 
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
+def add_attribute(obj, name, value):
+    """
+    Function that adds a new attribute to an object if it’s possible
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
 
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    setattr(obj, name, value)
+
+
+if __name__ == "__main__":
+    add_attribute = __import__('101-add_attribute').add_attribute
