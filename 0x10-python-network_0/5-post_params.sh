aee4451ab10fc1cbd9d699026620eaf1e0b46660
shikoliamos31 <amosshikoli@gmail.com>
2024-03-27 23:18:56 -0700

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Define variables
-URL="$1"
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-
-# Send POST request with curl
-RESPONSE=$(curl -s -X POST "$URL" -d "email=$EMAIL" -d "subject=$SUBJECT")
-
-# Display response body
-echo "POST params:"
-echo "$RESPONSE"
-
+# Sends a POST request to the passed URL, and displays the body of the response
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
