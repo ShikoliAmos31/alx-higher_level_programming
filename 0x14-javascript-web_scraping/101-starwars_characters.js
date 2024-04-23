e2692ff2da37d1388fae7ab907caf5ecf1065545
shikoliamos31 <amosshikoli@gmail.com>
2024-04-23 12:04:59 -0700

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,31 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
   } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
+    const filmData = JSON.parse(body);
+    const charactersUrls = filmData.characters;
+
+    charactersUrls.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
         if (error) {
-          console.log(error);
+          console.error('Error:', error);
+        } else if (response.statusCode !== 200) {
+          console.error('Status:', response.statusCode);
         } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
         }
       });
-    }
+    });
   }
 });
